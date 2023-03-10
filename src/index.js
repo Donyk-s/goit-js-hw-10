02c7283316c9cf7a32c1a03106cad593ac0a8c0a
Sergey Donyk <doncarlos@i.ua>
2023-03-11 01:10:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const refs = { searching: document.querySelector('#search-box') };
+console.log(refs.searching);
+refs.searching.addEventListener('keydown', e => {});
+fetch(
+  `https://restcountries.com/v2/name/{name}?fullText=true,capital/{capital},population,flags.svg,languages`
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+  })
+  .catch(err => {
+    console.log(err);
+  });
