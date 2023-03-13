c4e2dd982370dfe640936dafb3a19744057afdd9
Sergey Donyk <doncarlos@i.ua>
2023-03-14 00:01:42 +0200

    addFetch

--- src/index.js
+++ src/index.js
@@ -31,13 +31,19 @@ refs.searching.addEventListener(
     }
   }, DEBOUNCE_DELAY)
 );
-fetch(`https://restcountries.com/v2/name/{name}?fullText=true`)
+fetch(
+  `https://restcountries.com/v3.1/name/{name}?fields=,name,capital,population,flags,languages`
+)
   .then(response => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        return [];
+      }
+      throw new Error(response.status);
+    }
     return response.json();
   })
-  .then(country => {
-    console.log(country);
-  })
+
   .catch(err => {
-    console.log(err);
+    console.error(err);
   });
