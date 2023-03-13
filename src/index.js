68594938667118b990d8f36895ddef4a024fdd46
Sergey Donyk <doncarlos@i.ua>
2023-03-13 23:46:06 +0200

    add_notiflix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const refs = {
@@ -9,7 +10,27 @@ const refs = {
 console.log(refs.countryInfo);
 console.log(refs.countryList);
 console.log(refs.searching);
-refs.searching.addEventListener('keydown', e => {});
+refs.searching.addEventListener(
+  'input',
+  debounce(e => {
+    const valueTrimmed = input.value.trim();
+    cleanHtml();
+    if (valueTrimmed !== '') {
+      fetchCountries(valueTrimmed).then(response => {
+        if (response.lenght > 10) {
+          Notiflix.Notify.info('Search is bad');
+        } else if (response === undefined) {
+          // або тут записати 0
+          Notiflix.Notify.info('undefined');
+        } else if (response.lenght >= 2 && response <= 10) {
+          renderCountryList(response);
+        } else if (response.lenght === 1) {
+          renderOneCountry(response);
+        }
+      });
+    }
+  }, DEBOUNCE_DELAY)
+);
 fetch(`https://restcountries.com/v2/name/{name}?fullText=true`)
   .then(response => {
     return response.json();
